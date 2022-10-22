df7ff4eaca885b2a2894992fadbc7cd52f660570
Yurino Oga <yurino.o@latona.io>
2022-10-22 17:54:33 +0900

    release

--- data-platform-payment-method-sql-payment-method-data-setup.sql
+++ data-platform-payment-method-sql-payment-method-data-setup.sql
@@ -1,8 +1,9 @@
 LOCK TABLES `data_platform_payment_method_payment_method_data` WRITE;
 
-INSERT INTO `data_platform_payment_method_payment_method_data` (`PaymentMethod`)
+INSERT INTO `data_platform_payment_method_payment_method_data` (`Country`, `PaymentMethod`)
 VALUES
-	('T');
-	('F');
-	('I');
+	('JP', 'T');
+	('JP', 'F');
+	('JP', 'I');
+	('JP', 'S');
 UNLOCK TABLES;
--- data-platform-payment-method-sql-payment-method-data.sql
+++ data-platform-payment-method-sql-payment-method-data.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `data_platform_payment_method_payment_method_data`
 (
-    `PaymentMethod`                  varchar(1) NOT NULL,
-    PRIMARY KEY (`PaymentMethod`)
+    `Country`              varchar(3) NOT NULL,
+    `PaymentMethod`        varchar(1) NOT NULL,
+    PRIMARY KEY (`Country`, `PaymentMethod`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
