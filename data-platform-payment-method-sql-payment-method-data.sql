b09d7b9748211bd67d52d005b588557db0d77e2e
kimura <creative@latona.io>
2023-05-16 17:56:47 +0900

    release

--- data-platform-payment-method-sql-payment-method-data.sql
+++ data-platform-payment-method-sql-payment-method-data.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `data_platform_payment_method_payment_method_data`
 (
     `PaymentMethod`        varchar(1) NOT NULL,
+    
     PRIMARY KEY (`PaymentMethod`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
